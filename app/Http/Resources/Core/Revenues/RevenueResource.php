4e400a1251d66bce067f3de8b6b8fde5d39fbb09
Yan Gonçalves <yanw100@gmail.com>
2023-05-03 17:47:44 -0300

    feature: Added resource for Revenue

---
+++ app/Http/Resources/Core/Revenues/RevenueResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Core\Revenues;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RevenueResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "description" => $this->description,
+            "value" => $this->value,
+            "received_at" => $this->received_at,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at
+        ];
+    }
+}
