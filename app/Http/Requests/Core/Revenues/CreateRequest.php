026e8034cbd0e4c67f8ccb377c53cdf90498d310
Yan Gonçalves <yanw100@gmail.com>
2023-05-05 22:42:07 -0300

    refactor: Changed request name to reflect its purpose

--- app/Http/Requests/Core/Revenues/CreateRequest.php
+++ app/Http/Requests/Core/Revenues/CreateRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Validator;
 
-class RevenueRequest extends FormRequest
+class CreateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
