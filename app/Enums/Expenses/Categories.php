a7a8840e15b3b24ef53ab5ef2944e12fbb4e16da
Yan Gonçalves <yanw100@gmail.com>
2023-05-10 18:29:00 -0300

    feature: Create enum for categories. - Added bensampo enum package.

---
+++ app/Enums/Expenses/Categories.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums\Expenses;
+
+use App\Models\Category;
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static ALIMENTACAO()
+ * @method static static SAUDE()
+ * @method static static MORADIA()
+ * @method static static TRANSPORTE()
+ * @method static static EDUCACAO()
+ * @method static static LAZER()
+ * @method static static IMPREVISTOS()
+ * @method static static OUTROS()
+ */
+final class Categories extends Enum
+{
+    const ALIMENTACAO = 'Alimentação';
+    const SAUDE = 'Saúde';
+    const MORADIA = 'Moradia';
+    const TRANSPORTE = 'Transporte';
+    const EDUCACAO = 'Educação';
+    const LAZER = 'Lazer';
+    const IMPREVISTOS = 'Imprevistos';
+    const OUTROS = 'Outros';
+
+    public static function getCategoryId(string $categoryName = self::OUTROS): int
+    {
+        return Category::where('name', $categoryName)->first()->id ?? 8;
+    }
+}
