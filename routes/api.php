dba4984d28b08048d6fc79b6442e212125137708
Yan Gonçalves <yanw100@gmail.com>
2023-05-16 14:46:12 -0300

    refactor: Moved routes to auth middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Core\AuthController;
 use App\Http\Controllers\Core\ExpensesController;
 use App\Http\Controllers\Core\RevenuesController;
 use App\Http\Controllers\ResumeController;
@@ -17,13 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('revenues', RevenuesController::class);
-Route::get('revenues/extract/{year}/{month}', [RevenuesController::class, 'extract']);
-Route::resource('expenses', ExpensesController::class);
-Route::get('expenses/extract/{year}/{month}', [ExpensesController::class, 'extract']);
-Route::get('summary/{year}/{month}', [ResumeController::class, 'resume']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::resource('revenues', RevenuesController::class);
+    Route::get('revenues/extract/{year}/{month}', [RevenuesController::class, 'extract']);
+    Route::resource('expenses', ExpensesController::class);
+    Route::get('expenses/extract/{year}/{month}', [ExpensesController::class, 'extract']);
+    Route::get('summary/{year}/{month}', [ResumeController::class, 'resume']);
 });
 
