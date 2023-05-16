1b67f54088ce3f308e7d5b8ab1a69b1454090e33
Yan Gonçalves <yanw100@gmail.com>
2023-05-16 16:27:22 -0300

    feature: Implemented sign in method

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -4,12 +4,27 @@ namespace App\Services\Auth;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class AuthService
 {
+    /**
+     * @throws HttpException
+     */
     public function signIn(array $loginInfo): array
     {
-
+        $user = User::where('email', $loginInfo['email'])->first();
+        if (Hash::check($loginInfo['password'], $user->password)) {
+            $user->tokens()->delete();
+            $token = $user->createToken("{$user->email}." . time());
+            return [
+                'name' => $user->name,
+                'email' => $user->email,
+                'token' => $token->plainTextToken
+            ];
+        } else {
+            throw new HttpException(422, 'Wrong password');
+        }
     }
 
     public function signUp(array $registerInfo): User
