3b105d6b647efc72e4bfe76d33e4706f175379e9
Yan Gonçalves <yanw100@gmail.com>
2023-06-11 01:50:33 -0300

    feature: Added exception for user not found. - Add a catch for http exception and model not found

--- app/Http/Controllers/Core/AuthController.php
+++ app/Http/Controllers/Core/AuthController.php
@@ -9,7 +9,9 @@ use App\Http\Resources\UserResource;
 use App\Services\Auth\AuthService;
 use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class AuthController extends Controller
 {
@@ -28,6 +30,8 @@ class AuthController extends Controller
         try {
             $signInResponse = $this->authService->signIn($request->validated());
             return response()->json($signInResponse);
+        } catch (ModelNotFoundException|HttpException $e) {
+            throw $e;
         } catch (Exception $e) {
             Bugsnag::notifyException($e);
             throw $e;
